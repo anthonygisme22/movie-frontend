666187ca4588d18a970474f3c6e9612b0b178b29
anthonyging12 <65613944+anthonyging12@users.noreply.github.com>
2025-03-31 20:13:40 -0400

    commit for movie-frontend working

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,12 +2,15 @@
 import './globals.css';
 import NavBar from './components/NavBar';
 import { AuthProvider } from './context/AuthContext';
-
 import { ReactNode } from 'react';
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>My Movie DB</title>
+      </head>
       <body className="bg-gray-50 min-h-screen">
         <AuthProvider>
           <NavBar />
